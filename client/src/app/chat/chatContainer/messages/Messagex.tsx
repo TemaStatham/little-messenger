560e3aeb3adc4ff76838c1d958a02c6c4df12015
Karatist <skammoshenik@gmail.com>
2024-01-11 00:56:20 +0300

    сделал скрол для сообщений

--- client/src/app/chat/chatContainer/messages/Messagex.tsx
+++ client/src/app/chat/chatContainer/messages/Messagex.tsx
@@ -1,7 +1,7 @@
+import React, { useEffect, useRef } from 'react';
 import styles from './Messages.module.css';
 import { Data } from '../../../../types/Data';
 import { Chat, Message } from '../../../../types/Chats';
-import { useEffect } from 'react';
 import { User } from '../../../../types/User';
 
 type MessagesProps = {
@@ -12,34 +12,57 @@ type MessagesProps = {
 };
 
 export const Messages = (props: MessagesProps) => {
+  const messagesBlockRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     const token = localStorage.getItem('token');
-    if (token)
-      return () => {
-        props.handleEvent({
-          status: 'get messages',
-          token: token,
-          clientID: `${props.chat?.userID}`,
-          content: '',
-          chatId: `${props.chat?.chatID}`,
-        });
-      };
+    if (token) {
+      props.handleEvent({
+        status: 'get messages',
+        token: token,
+        clientID: `${props.chat?.userID}`,
+        content: '',
+        chatId: `${props.chat?.chatID}`,
+      });
+    }
   }, []);
+
+  useEffect(() => {
+    // Когда сообщения обновляются, прокрутите вниз
+    return () => {
+      if (messagesBlockRef.current) {
+        messagesBlockRef.current.scrollTop =
+          messagesBlockRef.current.scrollHeight;
+      }
+    };
+  }, [props.messages]);
+
+  const scrollToTop = () => {
+    if (messagesBlockRef.current) {
+      messagesBlockRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  };
 
   return (
     <>
       {props.messages ? (
-        <div className={styles.messages}>
+        <div
+          className={styles.messages}
+          onClick={scrollToTop}
+          ref={messagesBlockRef}
+        >
           <div className={styles.messages__messages_block}>
             {props.messages.map((message) => {
-              if (message.userID == props.user.id)
-                return (
-                  <div className={styles.me} key={message.id}>
-                    {message.content}
-                  </div>
-                );
               return (
-                <div className={styles.opp} key={message.id}>
+                <div
+                  className={
+                    message.userID === props.user.id ? styles.me : styles.opp
+                  }
+                  key={message.id}
+                >
                   {message.content}
                 </div>
               );
